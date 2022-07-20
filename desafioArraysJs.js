a7964b98e935f4dfe267846a9e612c244fc1f18b
Ezequiel Jimenez <ezequielemilianojimenez@gmail.com>
2022-07-19 20:38:55 -0400

    Primer intento con For of

---
+++ desafioArraysJs.js
@@ -0,0 +1,43 @@
+
+  RECORRIENDO ARRAYS
+const setJaponese = ['Mesa', 'Mesa de luz', 'Armario', 'Kokatsu', 'Biblioteca de Mangas']
+
+
+console.log( setJaponese.includes('Kokatsu') ) 
+console.log( setJaponese.includes('Biblioteca de Mangas') ) 
+
+
+
+
+// Con For  INTENTO 1
+/*
+const setDeco = [{ idNumb: 123, idName:'Set Coreano' producto: 'Bar de jotsu', 'Mesa de comer', 'Bilbioteca' },
+                 { idNumb: 345, idName:'Set Patagónico' producto: 'Barra de madera', 'Mesa de picada', 'Vinoteca' },
+                 { idNumb: 567, idName:'Set Altiplano' producto: 'Mortero', 'Tabla para pachamalca', 'Cortina tehuelche' }];
+
+for (const producto of setDeco) {
+    console.log(producto.idName);
+    console.log(producto.idNumb);
+}
+
+*/
+//     Intento 2
+/*  
+
+const setDeco =  [{ idNumb: 123, idName:'Set Coreano' producto: 'Bar de jotsu'},
+                  { idNumb: 345, idName:'Set Patagónico' producto: 'Barra de madera'},
+                  { idNumb: 567, idName:'Set Altiplano' producto: 'Mortero'}];
+
+
+for (const producto of setDeco) {
+    console.log(producto.idNumb);
+    console.log(producto.idName);
+    console.log(producto.producto);
+} 
+
+*/
+
+
+
+
+
